236f3a5290676718c94f9b5a8c5c588dbe136c42
TopoVV <vladtopov2001@gmail.com>
2020-11-02 17:11:56 +0200

    H2 db configured for development

--- src/main/java/com/topov/usermanagement/model/User.java
+++ src/main/java/com/topov/usermanagement/model/User.java
@@ -3,14 +3,27 @@ package com.topov.usermanagement.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
+@Entity
+@Table(name = "app_users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    @Column
     private String firstName;
+    @Column
     private String lastName;
+    @Column
     private String birthDate;
+    @Column
     private String login;
+    @Column
     private String password;
+    @Column
     private String about;
-    private Address address;
+    //private Address address;
 }
