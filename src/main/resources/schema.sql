8cdb744a218d71e102d71454ed255afbc74287cf
TopoVV <vladtopov2001@gmail.com>
2020-11-03 14:23:15 +0200

    SQL data initialization on startup

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS app_users;
+DROP SEQUENCE IF EXISTS user_id_seq;
+
+CREATE SEQUENCE user_id_seq START 1 NO MAXVALUE;
+
+CREATE TABLE app_users (
+    user_id INT UNIQUE,
+    first_Name VARCHAR(255),
+    last_name VARCHAR(255),
+    birth_date DATE,
+    login VARCHAR(255) UNIQUE,
+    password VARCHAR(255),
+    about VARCHAR(255),
+    country VARCHAR(255),
+    city VARCHAR(255),
+    street VARCHAR(255),
+    house_no INT,
+
+    PRIMARY KEY(user_id)
+);
